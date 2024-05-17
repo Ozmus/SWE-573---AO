e7ee6d47a55d975e8f6e73f8c545f4a991d75a04
Ozmus <ahmetozmus2018@gmail.com>
2024-05-17 23:44:23 +0300

    User profile is created.

--- src/main/java/com/example/communityapplication/dao/ContentDao.java
+++ src/main/java/com/example/communityapplication/dao/ContentDao.java
@@ -2,12 +2,14 @@ package com.example.communityapplication.dao;
 
 import com.example.communityapplication.model.Content;
 import com.example.communityapplication.model.ContentTemplate;
+import com.example.communityapplication.model.User;
 
 import java.util.List;
 
 public interface ContentDao {
     Content findByContentId(int id);
     List<Content> findByContentTemplateId(ContentTemplate contentTemplateId);
+    List<Content> findByUser(User user);
     Content save(Content theContent);
     List<Content> searchByTitle(String title);
 }
